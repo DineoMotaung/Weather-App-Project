ab18a89c2e3303b4566a0f4974c6badfef8ed8c3
DineoMotaung <131559361+DineoMotaung@users.noreply.github.com>
2023-05-22 16:43:42 +0200

    Updated weather details

--- src/app.js
+++ src/app.js
@@ -27,9 +27,9 @@ function showTemperature(response) {
   document.querySelector("#current-temp").innerHTML = Math.round(
     response.data.main.temp
   );
-
   document.querySelector("#description").innerHTML =
-    response.data.weather[0].main;
+    response.data.weather[0].description;
+
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
